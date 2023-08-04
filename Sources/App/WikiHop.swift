0944fa04f3b0d1af34208d5278e99af56812a841
Dan Stepanov <dan@danstepanov.com>
2023-08-04 15:04:22 -0700

    add supabase client

--- Sources/App/WikiHop.swift
+++ Sources/App/WikiHop.swift
@@ -1,10 +1,13 @@
 import SwiftUI
+import Supabase
 
 import LibAuth
 import LibEngine
 import LibHopClient
 
 public struct WikiHopApp: App {
+    let client = SupabaseClient.live
+
     public init() {}
 
     public var body: some Scene {
@@ -14,5 +17,14 @@ public struct WikiHopApp: App {
                 .with(authClient: .live)
                 .with(hopClient: .live(url: URL(string: "https://mvgfizogxujtoxxskaue.supabase.co")!, key: ""))
         }
+    }
+}
+
+extension SupabaseClient {
+    static var live: Self {
+        SupabaseClient(
+            supabaseURL: URL(string: "https://mvgfizogxujtoxxskaue.supabase.co")!,
+            supabaseKey: ""
+        )
     }
 }
