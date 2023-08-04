57b39a9aeb3d692d5c5a12a43df4fb24f5f0e897
Jeff Boek <jeff@jeffboek.com>
2023-08-04 12:36:52 -0700

    Initial Commit

---
+++ WikiHop/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WikiHop
+//
+//  Created by Jeff Boek on 8/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WikiHop/WikiHopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiHopApp.swift
+//  WikiHop
+//
+//  Created by Jeff Boek on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WikiHopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
