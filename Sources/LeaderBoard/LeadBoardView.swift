e89549e2f1ea585d3208c77728274d16e314d72e
maxdemaio <maxdman321@gmail.com>
2023-08-05 22:42:50 -0400

    init leader board view

---
+++ Sources/LeaderBoard/LeadBoardView.swift
@@ -0,0 +1,20 @@
+//
+//  LeadBoardView.swift
+//  WikiHop
+//
+//  Created by Maxwell DeMaio on 8/5/23.
+//
+
+import SwiftUI
+
+struct LeadBoardView: View {
+    var body: some View {
+        Text("Hello, Leader Board!")
+    }
+}
+
+struct LeadBoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        LeadBoardView()
+    }
+}
