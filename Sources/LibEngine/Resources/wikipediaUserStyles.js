882557e8e928e913cc0942a71cedf33192992f7b
metal-face <hughesbryan3000@gmail.com>
2023-08-05 22:30:34 -0400

    Wikipedia search icon removed
    Icon removed so user cannot use it to search for what they're looking force, forces user to use our search.

--- Sources/LibEngine/Resources/wikipediaUserStyles.js
+++ Sources/LibEngine/Resources/wikipediaUserStyles.js
@@ -2,11 +2,15 @@ console.log("boek loaded 2")
 var style = document.createElement('style');
 style.innerHTML = `
 .content_block, .collapsible-block {
-		display: block !important;
+        display: block !important;
 }
 .section_heading button {
-		display: none !important;
+        display: none !important;
 }
 `;
 console.log("boek loaded 2", style)
 document.head.appendChild(style)
+
+var searchIcon = document.getElementById("searchIcon");
+searchIcon.style.display = "none";
+
